8987201c21920cffcf632dd25f34e4c7cb210bc4
PrestonSSD2 <preston_tighe@yahoo.com>
2016-09-17 08:01:18 -0500

    docker work

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -10,52 +10,52 @@ sudo apt-get -y update
 #
 ## Essentials
 sudo apt-get install -y curl wget build-essential python-software-properties python g++ make git-core libkrb5-dev vim ssh
-#
-## -----------------------------------------------------------------------SERVER, PHP--------------------------------------------------------------------------------
-## Main lamp
-#sudo apt-get install -y lamp-server^
-#
-## Set root Apache folder to vagrant root folder
-#if ! [ -L /var/www ]; then
-#  rm -rf /var/www
-#  ln -fs /vagrant /var/www
-#fi
-#
-## PHP
-#sudo apt-get install -y php5-xdebug php5-curl
-#
-## XDebug config
-#cat << EOF | sudo tee -a /etc/php5/conf.d/xdebug.ini
-#zend_extension=/usr/lib/php5/20090626/xdebug.so
-#xdebug.scream=1
-#xdebug.cli_color=1
-#xdebug.show_local_vars=1
-#xdebug.idekey= PHPSTORM
-#xdebug.remote_autostart=1
-#xdebug.remote_enable=1
-#EOF
-#
-#cat << EOF | sudo tee -a /etc/apache2/conf.d/xdebug.conf
-#ServerName localhost
-#EOF
-#
-## Apache mods
-#sudo a2enmod rewrite
-#sudo a2enmod headers
-#sudo a2enmod ssl
-#
-## PHP Config
-#sed -i "s/post_max_size = .*/post_max_size = 12M/" /etc/php5/apache2/php.ini
-#sed -i "s/upload_max_filesize = .*/upload_max_filesize = 10M/" /etc/php5/apache2/php.ini
-#
-#sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-#
-## Apache restart after changes
-#sudo service apache2 restart
-#
-## PHP package manager
-#curl -sS https://getcomposer.org/installer | php
-#sudo mv composer.phar /usr/local/bin/composer
+
+# -----------------------------------------------------------------------SERVER, PHP--------------------------------------------------------------------------------
+# Main lamp
+sudo apt-get install -y lamp-server^
+
+# Set root Apache folder to vagrant root folder
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+# PHP
+sudo apt-get install -y php5-xdebug php5-curl
+
+# XDebug config
+cat << EOF | sudo tee -a /etc/php5/conf.d/xdebug.ini
+zend_extension=/usr/lib/php5/20090626/xdebug.so
+xdebug.scream=1
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+xdebug.idekey= PHPSTORM
+xdebug.remote_autostart=1
+xdebug.remote_enable=1
+EOF
+
+cat << EOF | sudo tee -a /etc/apache2/conf.d/xdebug.conf
+ServerName localhost
+EOF
+
+# Apache mods
+sudo a2enmod rewrite
+sudo a2enmod headers
+sudo a2enmod ssl
+
+# PHP Config
+sed -i "s/post_max_size = .*/post_max_size = 12M/" /etc/php5/apache2/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 10M/" /etc/php5/apache2/php.ini
+
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
+# Apache restart after changes
+sudo service apache2 restart
+
+# PHP package manager
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
 # -----------------------------------------------------------------------NODE--------------------------------------------------------------------------------
 # Node JS w/ NPM
@@ -70,7 +70,7 @@ npm set registry https://registry.npmjs.org/
 # Grunt, Bower, & Ember CLI
 #sudo npm install -g grunt-cli
 #sudo npm install -g bower
-sudo npm install -g ember-cli
+#sudo npm install -g ember-cli
 
 # Install Bower & NPM components
 cd /vagrant/ember-app
