7cc5450f00e79b2b73cb3959b02950968638334e
Preston Tighe <preston_tighe@yahoo.com>
2016-09-18 04:37:35 -0500

    Remove application logs directory

--- application/logs/log-2016-09-17.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-DEBUG - 2016-09-17 23:18:46 --> Config Class Initialized
-DEBUG - 2016-09-17 23:18:46 --> Hooks Class Initialized
-DEBUG - 2016-09-17 23:18:46 --> Utf8 Class Initialized
-DEBUG - 2016-09-17 23:18:46 --> UTF-8 Support Enabled
-DEBUG - 2016-09-17 23:18:46 --> URI Class Initialized
-DEBUG - 2016-09-17 23:18:46 --> Router Class Initialized
-ERROR - 2016-09-17 23:18:46 --> 404 Page Not Found --> welcome
