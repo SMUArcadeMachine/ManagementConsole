f3b74411508084e5cd7905ffaa63af334cf808c7
DejayJD <jdfrancis@smu.edu>
2016-09-26 17:01:37 -0500

    Added image covers and polished the UI a bit

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,15 +1,22 @@
 import Ember from 'ember';
 
+Ember.$(document).ready(function() {
+	Ember.$(".rom").click(function() {
+		var target = Ember.$(this).attr('data-target');
+		Ember.$('#'+target).modal('show');
+	});
+});
 export default Ember.Route.extend({
 	model() {
+		// This returns undefined - console.log(this.get('private-9c66cc-managementconsole.apiary-mock.com/getroms'));
 		return [
-		 {'title':'Super Mario Bros', 'console':'NES'},
-		 {'title':'Pac Man', 'console':'NES'},
-		 {'title':'Galaga', 'console':'NES'},
-		 {'title':'Frogger', 'console':'NES'},
-		 {'title':'Marvel vs. Capcom', 'console':'Standalone'},
-		 {'title':'The Legend of Zelda', 'console':'NES'},
-		 {'title':'CastleVania', 'console':'NES'},
-		 {'title':'Final Fantasy II', 'console':'NES'}];
+		 {'title':'SuperMarioBros','prettytitle':'Super Mario Bros', 'console':'NES'},
+		 {'title':'PacMan','prettytitle':'Pac Man', 'console':'NES'},
+		 {'title':'Galaga','prettytitle':'Galaga', 'console':'NES'},
+		 {'title':'Frogger','prettytitle':'Frogger', 'console':'NES'},
+		 {'title':'MarvelVSCapcom','prettytitle':'Marvel vs. Capcom', 'console':'Standalone'},
+		 {'title':'TheLegendOfZelda','prettytitle':'The Legend of Zelda', 'console':'NES'},
+		 {'title':'CastleVania','prettytitle':'CastleVania', 'console':'NES'},
+		 {'title':'FinalFantasyII','prettytitle':'Final Fantasy II', 'console':'NES'}];
 	}
 });
