dcd8ed274643153dd80666579198ea8b5e9e137b
Clay <cplewis@smu.edu>
2016-09-26 23:41:07 -0500

    Rom model file creation, just doing this so I can merge

---
+++ application/models/rom_model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claylewis
+ */
+
+class Rom_model extends CI_Model {
+
+    function get_active_roms(){
+        //$activeRoms = $this->db->que
+
+    }
+
+
+
+}
