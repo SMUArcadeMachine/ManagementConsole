d6609ece8b68955816f5e907d1e7d037541e7d56
AlyssaR <arahman@smu.edu>
2016-09-29 01:57:11 +0600

    adding semaphore protection to reboot.php & require integration of or with gamedata.php

--- php/reboot.php
+++ php/reboot.php
@@ -1,7 +1,21 @@
 <?php
+$MEMSIZE = 512;
+$SEMKEY = 1;
+$SHMKEY = 2;
+
+$sem_id = sem_get(SEM_KEY);
+
+//Z's stuff should go here
+
+
+
 
 if($_POST['run']) {
-	shell_exec("sudo /sbin/shutdown -r now");
+	//if( sem_acquire($sem_id)){
+	shell_exec("sudo /sbin/shutdown -r +1");
+	echo "Releasing semaphore";
+	sem_release($sem_id);
+	//	}
 }
 
-?>
+?> 
