56fb133f39bc0cb352a0bf950b7e4319eb68dcb2
David Kim <dykim@smu.edu>
2016-09-29 11:07:54 -0500

    activates roms

--- app/components/rom-modal.js
+++ app/components/rom-modal.js
@@ -2,15 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    updateRoms() {
-      let checked = $('input:checked');
-      console.log(checked);
+    updateRoms(model) {
+      // console.log(model);
+      // console.log(model[0].get('title'));
+      var checked = $('input:checked');
+      var json = new Object();
+      json.activate = [];
       if(checked.length >= 0) {
-        var objects = this.get('model.active');
-        for(var i = 0;i < checked.length; i++) {
-          console.log(checked[i].value);
+        var game = new Object();
+        for(var i = 0; i < checked.length; i++) {
+          var splitString = (checked[i].value).split("|");
+          game.title = splitString[0];
+          game.console = splitString[1];
         }
+        json.activate.push(game);
       }
+  		let url = "https://private-50f0c-digarcademachine1.apiary-mock.com/activate";
+  		var response = Ember.$.post(url, json);
+  		console.log(response);
     }
   }
 });
