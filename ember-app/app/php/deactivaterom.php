a95920f602c8906f49673b6e10c1cde2e813621f
Clay <cplewis@smu.edu>
2016-09-29 13:51:45 -0500

    change deativate to reg rename, not ssh rename

--- ember-app/app/php/deactivaterom.php
+++ ember-app/app/php/deactivaterom.php
@@ -18,22 +18,16 @@ $roms = json_decode($roms);
 $roms = $roms["deactivate"];
 
 // Connect to the game pi with ssh
-$connection = ssh2_connect('The Other Pi Address', 22);
-ssh2_auth_password($connection, 'pi', 'raspberry');
-$sftp = ssh2_sftp($connection);
-
 
 foreach($roms as $rom){
     // Get the file location and name
     $stmt = $db->prepare("SELECT file_name FROM roms WHERE game_name = ?");
     $stmt->execute(array($rom["title"]));
     $romRecord = $stmt->fetch(PDO::FETCH_ASSOC);
-//    $fileLoc = $romRecord["GameRomLoc"];
-//    $fileName = substr(strrchr($fileLoc, "/"), 1);
     $fileName = $romRecord["file_name"];
 
     // Unhide the file
-    ssh2_sftp_rename($sftp, "/home/pi/RetroPie/roms/mame-mame4all/{$fileName}", "/home/pi/gamestorage/{$fileName}");
+    rename("/home/pi/RetroPie/roms/mame-mame4all/{$fileName}", "/home/pi/gamestorage/{$fileName}");
     $stmt = $db->prepare("UPDATE roms SET rom_active = 0 WHERE game_name = ?");
     $stmt->execute(array($rom["title"]));
 }
