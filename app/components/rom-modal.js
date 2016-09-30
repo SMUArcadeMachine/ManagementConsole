853214c0be4a788ead2ad07126a6b1b07fe47951
David Kim <dykim@smu.edu>
2016-09-30 12:05:11 -0500

    added filtering for the inactive rom search

--- app/components/rom-modal.js
+++ app/components/rom-modal.js
@@ -15,11 +15,35 @@ export default Ember.Component.extend({
           game.console = splitText[1];
           json.games.push(game);
         }
+      });
 
   		let url = "http://192.168.1.7/php/activaterom.php";
       console.log(json);
   		var response = Ember.$.post(url, json);
   		console.log(response);
+    },
+    filter() {
+      var inactiveBlocks = $('.inactiveDiv');
+      var inactives = $('span.inactivetitle');
+      if(inactives.length > 0) {
+        var filterString = this.get('filterValue').toLowerCase();
+        if(filterString != "") {
+          for(var i = 0; i < inactives.length; i++) {
+            var inactiveName = inactives[i].innerText;
+            if(inactiveName.toLowerCase().includes(filterString)) {
+              inactiveBlocks[i].style.display = "block";
+            }
+            else {
+              inactiveBlocks[i].style.display = "none";
+            }
+          }
+        }
+        else {
+          for(var i = 0; i < inactives.length; i++) {
+            inactiveBlocks[i].style.display = "block";
+          }
+        }
+      }
     }
   }
 });
