8c6ac9c2449182e9ff00adfb579b87869c298c22
ClayLewis <cplewis@smu.edu>
2016-09-30 13:34:33 -0500

    Update deactivaterom.php

--- php/deactivaterom.php
+++ php/deactivaterom.php
@@ -12,23 +12,25 @@ $password = "8043v36m807c3084m6m03v";
 
 $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-// Get the roms to be deactivated
-$roms = $_GET["games"];
+// Get the roms to be activated
+$roms = file_get_contents('php://input');
+//echo var_dump($_POST);
+//echo "We receive: ".$roms;
 $roms = json_decode($roms);
-$roms = $roms["deactivate"];
-
-
-
+$roms = $roms->games;
+//echo $roms;
+$res = array();
 foreach($roms as $rom){
+    echo $rom->title;
+    // Get the file location and name
     $stmt = $db->prepare("SELECT file_name FROM roms WHERE game_name = ?");
-    $stmt->execute(array($rom["title"]));
+    $stmt->execute(array($rom->title));
     $romRecord = $stmt->fetch(PDO::FETCH_ASSOC);
     $fileName = $romRecord["file_name"];
-
     // Unhide the file
-    rename("/home/pi/RetroPie/roms/mame-mame4all/{$fileName}", "/home/pi/gamestorage/{$fileName}");
+    $res[0] = rename("/home/pi/RetroPie/roms/mame-mame4all/$fileName", "/home/pi/gamestorage/$fileName");
     $stmt = $db->prepare("UPDATE roms SET rom_active = 0 WHERE game_name = ?");
-    $stmt->execute(array($rom["title"]));
+    $res[1] = $stmt->execute(array($rom->title));
 }
-
-echo 1;
+$res = json_encode($res);
+echo $res;
