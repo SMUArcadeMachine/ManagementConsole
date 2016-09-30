cbdaf332432aac48cd6624ba14fe10949b4b0633
ClayLewis <cplewis@smu.edu>
2016-09-30 13:32:22 -0500

    Update activaterom.php

--- php/activaterom.php
+++ php/activaterom.php
@@ -25,14 +25,14 @@ foreach($roms as $rom){
     echo $rom->title;
     // Get the file location and name
     $stmt = $db->prepare("SELECT file_name FROM roms WHERE game_name = ?");
-    $stmt->execute(array($rom["title"]));
+    $stmt->execute(array($rom->title));
     $romRecord = $stmt->fetch(PDO::FETCH_ASSOC);
     $fileName = $romRecord["file_name"];
 
     // Unhide the file
     $res[0] = rename("/home/pi/gamestorage/{$fileName}", "/home/pi/RetroPie/roms/mame-mame4all/{$fileName}");
     $stmt = $db->prepare("UPDATE roms SET rom_active = 1 WHERE game_name = ?");
-    $res[1] = $stmt->execute(array($rom["title"]));
+    $res[1] = $stmt->execute(array($rom->title));
 }
 
 $res = json_encode($res);
