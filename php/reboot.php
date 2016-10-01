3cf196810737712474ee3e075c864797cc8f57d5
Kristofor Horst <KristoforHorst@users.noreply.github.com>
2016-09-30 21:33:36 -0500

    Update reboot.php

--- php/reboot.php
+++ php/reboot.php
@@ -14,8 +14,9 @@ $sem_id = sem_get(SEM_KEY, 1, 0666, 1);
 if($_POST['run']) {
 	if( sem_acquire($sem_id)){
 	shell_exec("sudo /sbin/shutdown -r +1");
-	echo "Releasing semaphore";
+	
 	sem_release($sem_id);
+	echo "Releasing semaphore";
 		}
 }
 
