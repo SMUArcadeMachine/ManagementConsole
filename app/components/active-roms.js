82ff6c383551052d4af61690f4ed180d9018927a
Bryce Stevenson <bstevenson@smu.edu>
2016-10-01 14:56:45 -0500

    changes

--- app/components/active-roms.js
+++ app/components/active-roms.js
@@ -2,13 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    showModal(modalName) {
-      var controller = this.controllerFor(modalName);
+    showModal: function() {
       console.log("In active-rom component js");
-      this.render(modalName, {
-        into: 'dashboad',
-        outlet: 'modal',
-      });
+      this._controller.send('closeModal');
+      console.log("In active-rom component js");
     }
   }
 });
