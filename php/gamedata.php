7c80d11ab45c6c5caa85c87e0f046946afddb8e1
Ziga Cerkovnik <zcerkovnik@smu.edu>
2016-10-02 16:19:20 -0500

    update gamedata.php

--- php/gamedata.php
+++ php/gamedata.php
@@ -36,21 +36,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $line = $_POST['postVariable'];
     global $time_start;
-    if (preg_match($reg['start'], $line, $matches)) {
+    preg_match($reg['start'], $line, $matches)
+	    echo $matches[0];
+    if ($matches[0] == "START")) {
 	  //  if(sem_acquire($sem_id)){ //set the semaphore here so we can't restart the Pi until the game is closed
         $time_start = preg_match($reg['time_start'], $line, $matches);
+	$myfile = fopen("temp_buffer.txt", "w") or die("Unable to open buffer file!");
+	fwrite($myfile, $matches[0]);
+	fclose($myfile);
+	/*try {
+		 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		 $sqlquery = 'SELECT * FROM gameData WHERE gameName="' . $game_name . '";';
+		 $temp = $conn->prepare($sqlquery);
+		 $temp->execute();
+		 $temp = $temp->fetchAll(PDO::FETCH_ASSOC);
+	
+	    if (!(count($temp) > 0)) {
+			$sqlquery = 'INSERT INTO gameData (timeStart) VALUES ("' . $time_start . '";';
+			$temp = $conn->prepare($sqlquery);
+			$temp->execute();
+			$temp = $temp->fetchAll(PDO::FETCH_ASSOC);
+		    } else {
+			$sqlquery = 'UPDATE gameData SET timeStart ="' . $time_startSQL . '";';
+			$temp = $conn->prepare($sqlquery);
+			$temp->execute();
+			$temp = $temp->fetchAll(PDO::FETCH_ASSOC);
+		    }
+            echo json_encode(array('success' => 'yes'));
+		//sem_release($sem_id); //We release the semaphore so that a restart can run if needed
+        } catch (PDOException $e) {
+            echo $sqlquery . "<br>" . $e->getMessage();
+		//sem_release($sem_id); //We release the semaphore so that a restart can run if needed
+        }*/
+	    
 //	}
-    } else if (preg_match($reg['time_end'], $line, $matches)) {
-	echo #time_start;
-        preg_match($reg['time_end'], $line, $matches);
-	$time_end = $matches[1];
-        preg_match($reg['game_name'], $line, $matches);
-	$time_start = matches[1];
-     
-        //$time_startSQL = date('H:i:s', $time_start);
-        //$time_endSQL = date('H:i:s', $time_end);
-	//echo $time_start;
-        $time_playedSQL = $time_end - $time_start;
+    } else {
+	preg_match($reg['end'], $line, $matches)
+		echo $matches;
+    	if ($matches[0] == "END") {
+		preg_match($reg['time_end'], $line, $matches);
+		$time_end = $matches[0];
+		preg_match($reg['game_name'], $line, $matches);
+		$game_name = $matches[0];
+		echo $matches;
+		
+		$myfile = fopen("temp_buffer.txt", "r") or die ("Can't open buffer file");
+		$time_start = fgets($myfile);
+		fclose($myfile);
+		unlink($myfile);
+		$time_played = $time_end - $time_start;
+		//$time_startSQL = date('H:i:s', $time_start);
+		//$time_endSQL = date('H:i:s', $time_end);
+		//echo $time_start;
+		//$time_playedSQL = $time_end - $time_start;
 
    /* else if($_POST['restart']) { //If we see the restart value in the POST data, we will restart
    // else if(preg_match($reg['restart'],$line,$matches)) { //This looks for for restart pushed from the machine software side, to better integrate with gamedata.php current functionality.
@@ -61,41 +100,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	echo "Releasing semaphore";
 	}*/
        //$sqlquery = 'SELECT * FROM gameData WHERE gameName="' . $game_name . '";';
-        //store in db
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sqlquery = 'SELECT * FROM gameData WHERE gameName="' . $game_name . '";';
-            $temp = $conn->prepare($sqlquery);
-            $temp->execute();
+		
+        //update db when received END
+		try {
+		    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    $sqlquery = 'SELECT * FROM gameData WHERE gameName="' . $game_name . '";';
+		    $temp = $conn->prepare($sqlquery);
+		    $temp->execute();
+		    $temp = $temp->fetchAll(PDO::FETCH_ASSOC);
 
-/*            if(empty($temp)){
-                $query = "CREATE TABLE gameData(
-                          timeStart TIME,
-                          timeEnd TIME,
-                          timePlayed TIME,
-                          gameName VARCHAR(50),
-                          counts int(3000)
-                )";
-                $temp = $conn->prepare($query);
-                $temp->execute();
-
-                $temp = $conn->prepare($sqlquery);
-                $temp->execute();
-            }*/
-            $temp = $temp->fetchAll(PDO::FETCH_ASSOC);
-
-            if (!(count($temp) > 0)) {
-                $sqlquery = 'INSERT INTO gameData (timeStart,timeEnd,timePlayed,gameName,counts) VALUES ("' . $time_start . '","' . $time_end . '","' . $time_playedSQL . '","' . $game_name . '", 1;';
-                $temp = $conn->prepare($sqlquery);
-                $temp->execute();
-                $temp = $temp->fetchAll(PDO::FETCH_ASSOC);
-            } else {
-                $sqlquery = 'UPDATE gameData SET timeStart ="' . $time_startSQL . '", AND timeEnd="' . $time_endSQL . '", AND timePlayed = timePlayed+"' . $time_playedSQL . '", AND counts = counts+1 WHERE gameName ="' . $game_name . '";';
-                $temp = $conn->prepare($sqlquery);
-                $temp->execute();
-                $temp = $temp->fetchAll(PDO::FETCH_ASSOC);
-            }
+		    if (!(count($temp) > 0)) {
+			$sqlquery = 'INSERT INTO gameData (timeStart,timeEnd,timePlayed,gameName,counts) VALUES ("'.$time_start.'","'. $time_end . '","' . $time_played . '","' . $game_name . '", 1;';
+			$temp = $conn->prepare($sqlquery);
+			$temp->execute();
+			$temp = $temp->fetchAll(PDO::FETCH_ASSOC);
+		    } else {
+			$sqlquery = 'UPDATE gameData SET timeStart="'.$time_start.'" AND SET timeEnd="' . $time_end . '", AND SET timePlayed = timePlayed +"' .$time_played. '" - timeStart , AND SET counts = counts + 1 WHERE gameName ="' . $game_name . '";';
+			$temp = $conn->prepare($sqlquery);
+			$temp->execute();
+			$temp = $temp->fetchAll(PDO::FETCH_ASSOC);
+		    }
             echo json_encode(array('success' => 'yes'));
 		//sem_release($sem_id); //We release the semaphore so that a restart can run if needed
         } catch (PDOException $e) {
