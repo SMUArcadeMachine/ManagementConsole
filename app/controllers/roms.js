212b9dbaabf151a532b7fa364943f87589fa8549
David Kim <dykim@smu.edu>
2016-10-03 07:58:44 -0500

    finished drag drop functionality

--- app/controllers/roms.js
+++ app/controllers/roms.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  originals: Ember.computed.readOnly('model',function() {
+    return this.get('model');
+  }),
+  changes: Ember.A([]),
+  toInactive: Ember.A([]),
+  toActive: Ember.A([]),
   activeRoms: Ember.computed('model', function() {
     return this.get('model').actives;
   }),
@@ -11,24 +17,68 @@ export default Ember.Controller.extend({
   actions: {
     test() {
       console.log(this.get('activeRoms'));
+      console.log(this.get('originals'));
     },
     addActiveRom(rom) {
       var activeRoms = this.get('activeRoms');
       var inactiveRoms = this.get('inactiveRoms');
-      console.log(rom);
+      var changes = this.get('changes');
+      var toActive = this.get('toActive');
+      var toInactive = this.get('toInactive');
       if(!activeRoms.includes(rom)) {
         activeRoms.pushObject(rom);
         inactiveRoms.removeObject(rom);
+        if(changes.includes("-" + rom.title)) {
+          changes.removeObject("-" + rom.title);
+          toInactive.removeObject(rom);
+        }
+        else {
+          changes.pushObject("+" + rom.title);
+          toActive.pushObject(rom);
+        }
       }
     },
     addInactiveRom(rom) {
       var activeRoms = this.get('activeRoms');
       var inactiveRoms = this.get('inactiveRoms');
-      console.log(rom);
+      var changes = this.get('changes');
+      var toActive = this.get('toActive');
+      var toInactive = this.get('toInactive');
       if(!inactiveRoms.includes(rom)) {
         inactiveRoms.pushObject(rom);
         activeRoms.removeObject(rom);
+        if(changes.includes("+" + rom.title)) {
+          changes.removeObject("+" + rom.title);
+          toActive.removeObject(rom);
+        }
+        else {
+          changes.pushObject("-" + rom.title);
+          toInactive.pushObject(rom);
+        }
       }
+    },
+    pushChanges() {
+      var toActive = this.get('toActive');
+      var activeJson = new Object();
+      activeJson.games = [];
+      var toInactive = this.get('toInactive');
+      var inactiveJson = new Object();
+      inactiveJson.games = [];
+      //do activate logic here
+      for(var i = 0; i < toActive.length; i++) {
+        activeJson.games.push({"title": toActive[i].title, "console": toActive[i].console});
+      }
+      //do deactiivate logic here
+      for(var i = 0; i < toInactive.length; i++) {
+        inactiveJson.games.push({"title": toInactive[i].title, "console": toInactive[i].console});
+      }
+      console.log(activeJson);
+      console.log(inactiveJson);
+
+      let urlActive = "http://192.168.1.7/php/activaterom.php";
+      let urlDeactive = "http://192.168.1.7/php/deactivaterom.php";
+      var responseActive = Ember.$.post(url, activeJson);
+      var responseDeactive = Ember.$.post(url, inactiveJson);
     }
   }
 });
