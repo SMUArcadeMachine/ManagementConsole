c029e72f6332133ae6abdaa3b4561d46b5647fea
Preston Tighe <preston_tighe@yahoo.com>
2016-10-03 04:35:21 -0500

    Reboot merge with confirmation prompt

--- ember-app/app/components/main-header.js
+++ ember-app/app/components/main-header.js
@@ -7,7 +7,20 @@ export default Ember.Component.extend(NavigationMixin,{
             this.get('session').invalidate();
         },
         resetSystem(){
-            Ember.Logger.debug('Resetting the whole Arcade machine...TODO');
+            createConfirm(
+                'Reboot',
+                'Are you sure you want to reboot the Arcade Machine?',
+                () => {
+                    showLoader();
+                    this.get('ajax').post('/reboot').then(function(){
+                        createAlert('Rebooting Arcade Machine...','','success');
+                    }, (error) => {
+                        createError('Error rebooting Arcade Machine', this.get('utils').parse_error(error));
+                    }).finally(function(){
+                        hideLoader();
+                    });
+                }
+            );
         }
     }
 });
--- php/reboot.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if($_POST['run']) {
-	shell_exec("sudo sh /var/www/html/documentation/scripts/rbt");
-		
-}
