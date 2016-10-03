49328641b376e804bbfc00cada9f03eff57d5a36
Avery <aferrante@smu.edu>
2016-10-02 20:21:43 -0500

    updated PHP file to call the new working rbt script.

--- php/reboot.php
+++ php/reboot.php
@@ -1,23 +1,8 @@
 <?php
-//Reference to semaphore http://www.re-cycledair.com/php-dark-arts-semaphores
-$MEMSIZE = 512;
-$SEMKEY = 1;
-$SHMKEY = 2;
-
-$sem_id = sem_get(SEM_KEY, 1, 0666, 1);
-
-//Z's stuff should go here
-
-
-
 
 if($_POST['run']) {
-	if( sem_acquire($sem_id)){
-	shell_exec("sudo /sbin/shutdown -r +1");
-	
-	sem_release($sem_id);
-	echo "Releasing semaphore";
-		}
+	shell_exec("sudo sh /var/www/html/documentation/scripts/rbt");
+		
 }
 
 ?> 
